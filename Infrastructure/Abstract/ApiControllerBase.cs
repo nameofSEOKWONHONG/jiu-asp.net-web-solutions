4fe080bd1174534e6a70c8cdcb68126ae4dbb3aa
19066Seokwon <SWHong@ecounterp.co.kr>
2021-11-23 16:58:49 +0900

    update comment

--- Infrastructure/Abstract/ApiControllerBase.cs
+++ Infrastructure/Abstract/ApiControllerBase.cs
@@ -9,6 +9,13 @@ namespace Infrastructure.Abstract
     [Route("api/[controller]")]
     public abstract class ApiControllerBase<TController> : ControllerBase
     {
+        /*
+         * 생성자 의존성 주입으로 IServiceProvider를 받지 않는다.
+         * 위 행위가 필요하다면 HttpContext.RequestService.GetService<>();를 사용한다.
+         * 또한 HttpContext.RequestService로 의존성 주입을 할때 해당 개체의 인스턴스가 Scope라면
+         * 같은 인스턴스 Scope인지 확인해야 하고 같은 Scope를 강제해야 한다며 ScopeCreate를 사용하도록 한다.
+         * IServiceProvider 의존성 주입 방식은 권장되지 않는 방식.
+         */
         private ILogger<TController> _loggerInstance;
         private IMediator _mediatorInstance;
         /// <summary>
@@ -19,7 +26,7 @@ namespace Infrastructure.Abstract
         /// httpcontext로 의존성 주입 (CQRS)
         /// </summary>
         protected IMediator _mediator => _mediatorInstance ??= HttpContext.RequestServices.GetService<IMediator>();
-        
+
         protected bool TryValidate<TEntity>(TEntity model, out ActionResult result) where TEntity : class
         {
             if (!this.TryValidateModel(model))
