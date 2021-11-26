adb541997731760d6d3936b5ee47c8c6eb7dcaf3
SeokWon Hong <h20913@gmail.com>
2021-11-27 00:45:10 +0900

    update

---
+++ Infrastructure/KafakaConsumerBackgroundService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Infrastructure.Services
+{
+    public class KafakaConsumerBackgroundService : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ConsumerConfig _consumerConfig;
+        public KafakaConsumerBackgroundService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _consumerConfig = new ConsumerConfig
+            { 
+                GroupId = "test-consumer-group",
+                BootstrapServers = _configuration["KafkaConfiguration:Uri"],
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // earliest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };            
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using (var c = new ConsumerBuilder<Ignore, string>(_consumerConfig).Build())
+            {
+                c.Subscribe("test");
+                
+                try
+                {
+                    while (!stoppingToken.IsCancellationRequested)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(stoppingToken);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    c.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Infrastructure/Services/Kafka/ProducerService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Services.Kafka
+{
+    public class ProducerService
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProducerService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task CreateProducer()
+        {
+            var config = new ProducerConfig { BootstrapServers = _configuration["KafkaConfiguration:Uri"] };
+            
+            // If serializers are not specified, default serializers from
+            // `Confluent.Kafka.Serializers` will be automatically used where
+            // available. Note: by default strings are encoded as UTF8.
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync("test", new Message<Null, string> { Value="test" });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        } 
+    }
+}
