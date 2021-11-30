4dfeec8ef9ed8985d4389611e74b8ef93c0ef643
19066Seokwon <SWHong@ecounterp.co.kr>
2021-11-30 15:20:01 +0900

    update

---
+++ YarpWebApplication/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// TODO : 인증 및 로그 밸러스 구현하기
+// reference : https://microsoft.github.io/reverse-proxy/
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapReverseProxy();
+});
+
+app.Run();
